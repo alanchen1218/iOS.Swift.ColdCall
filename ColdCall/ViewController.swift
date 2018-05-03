a87d7b0ec7abf8cd75c01da2c55a66c946763a3c
Alan Chen <alanchen1218@gmail.com>
2018-05-02 21:46:13 -0700

    randonly display name from list and random number(1-5) when button is clicked

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var randNumber: UILabel!
+    
+    
+    let names = ["Alan","Anne", "Sophie","Anne Sophie", "Aaron", "Andy", "Winton", "Alex", "Jonathan", "Darren", "Lorenzo", "Jacky", "Kyle", "Samson", "Victor", "Vincent", "Sophia", "Janet", "Jessica"]
+    var current = 0
+    
+    @IBAction func callCooldButton(_ sender: UIButton) {
+        current = Int(arc4random_uniform(UInt32(names.count-1)))
+        if current < names.count-1
+        {
+            current += 1
+        }
+        else {
+            current = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        nameLabel.text = names[current]
+        let randomnumber = Int(arc4random_uniform(UInt32(5)+1))
+        if randomnumber == 1 || randomnumber == 2 {
+            randNumber.textColor = UIColor.red
+            randNumber.text = String(randomnumber)
+        }
+        else if randomnumber == 3 || randomnumber == 4 {
+            randNumber.textColor = UIColor.blue
+            randNumber.text = String(randomnumber)
+        }
+        else if randomnumber == 5 {
+            randNumber.textColor = UIColor.green
+            randNumber.text = String(randomnumber)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        button.layer.cornerRadius = 30
+        button.clipsToBounds = true
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
